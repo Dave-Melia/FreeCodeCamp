0fca410184a18d4eea0f1ce545f221e237c556a4
Dave Melia <retrospock@gmail.com>
2017-03-21 13:18:34 +0000

    latest solution

---
+++ 05. Basic JavaScript/88. Manipulating Complex Objects/script.js
@@ -0,0 +1,24 @@
+var myMusic = [{
+    "artist"      : "Billy Joel",
+    "title"       : "Piano Man",
+    "release_year": 1973,
+    "formats"     : [
+      "CS",
+      "8T",
+      "LP"
+    ],
+    "gold"         : true
+  },
+  {
+    "artist"      : "Ludovico Einaudi",
+    "title"       : "Una Mattina",
+    "release_year": 2004,
+    "formats"     : [
+      "CD",
+      "LP",
+      "DD"
+    ],
+    "gold"        : true
+  }
+  // Add record here
+];
